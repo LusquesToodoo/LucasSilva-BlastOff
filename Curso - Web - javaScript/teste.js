d57f439b87ebedc25d87d6403937fcb1dfbb05df
Lusques <lucas.carlos@toodoo.com.br>
2022-08-23 14:25:17 -0300

    JS - Tarefa 25

---
+++ Curso - Web - javaScript/teste.js
@@ -0,0 +1,46 @@
+const month = [
+  "Janeiro",
+  "Fevereiro",
+  "Março",
+  "Abril",
+  "Maio",
+  "Junho",
+  "Julho",
+  "Agosto",
+  "Setembro",
+  "Outubro",
+  "Novembro",
+  "Dezembro",
+];
+
+// function validaData(data) {
+//   let validacao;
+//   if(data.length != 10){
+//     validacao = true;
+//   }else{
+//     const dataSplit = data.split('/');
+//     dataSplit[0].length != 2? '' : validacao = true;
+//     dataSplit[1].length != 2? '' : validacao = true;
+//     dataSplit[2].length != 3? '' : validacao = true;
+//     console.log(dataSplit)
+//     console.log(validacao)
+//   }
+// }
+// debugger;
+// const month = [
+//   "Janeiro",
+//   "Fevereiro",
+//   "Março",
+//   "Abril",
+//   "Maio",
+//   "Junho",
+//   "Julho",
+//   "Agosto",
+//   "Setembro",
+//   "Outubro",
+//   "Novembro",
+//   "Dezembro",
+// ];
+// const date = "22/08/2022";
+
+// validaData(date);
