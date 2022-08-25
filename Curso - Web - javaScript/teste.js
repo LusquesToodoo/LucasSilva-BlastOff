934b400b361c1423364d2f5582928cb3f67b8bfd
Lusques <lucas.carlos@toodoo.com.br>
2022-08-25 11:11:39 -0300

    JS - tarefa 29

--- Curso - Web - javaScript/teste.js
+++ Curso - Web - javaScript/teste.js
@@ -1,23 +1,53 @@
-const input = '123.321.123-35'
-
-console.log(input.replace(/[^0-9]/g,''))
-//------------------------------------------
-
-// const cpf = '364.231.487-24'
-// // const ExpReg = /(\d{3})(\W)(\d{3})(\W)(\d{3})/g
-// const ExpReg = /(\d{3})(\W)(\d{3})(\W)(\d{3})(\W)(\d{2})/
-// // const obj = ExpReg.exec(cpf)
-// const obj = ExpReg.match(cpf)
-// obj.forEach((a)=>{
-//   console.log(a)
-// })
-
-//------------------------------------------
-function apenasNumeros(string)
-{
-var numsStr = string.replace(/[^0-9]/g,'');
-return parseInt(numsStr);
+class Retangulo {
+  getHeight = () => {
+    return this.height;
+  };
+  getWidth = () => {
+    return this.width;
+  };
+  setHeight = function (height) {
+    this.height = height;
+  };
+  setWidth = function (width) {
+    this.width = width;
+  };
+  calculateArea = () => {
+    return this.height * this.width + 'm²';
+  };
+  calculatePerimeter = () => {
+    return (this.height * 2 + this.width * 2) + 'm';
+  };
+  constructor(height, width) {
+    this.height = height;
+    this.width = width;
+  }
+}
+function input(description){
+  return prompt(description)
 }
 
+function validateValue(description, MinValue, maxValue) {
+  let value = parseFloat(input(description));
+  while (value < MinValue || isNaN(value) || value > maxValue) {
+    value = input(`Informe um valor entre ${MinValue} e ${maxValue || 'qualquer valor'}.\n`)
+  }
+  return value;
+}
 
-console.log(apenasNumeros('Aqui está um exemplo de string que só pegará os valores ! 10/10/1010'))
+const height = validateValue('Informe a altura do retângulo (m):')
+const width = validateValue('Informe a largura do retângulo (m): ')
+const retangulo = new Retangulo(height, width)
+
+console.log(`Nome do objeto criado: retangulo`)
+console.log(`Atriburos do objeto:`)
+console.log(`retangulo.height - armazena o valor da altura do objeto`)
+console.log(`retangulo.width - armazena o valor da largura do objeto`)
+console.log(`Atriburos do objeto:`)
+console.log(`Funções e parâmetros para testes: `)
+console.log(`retangulo.setHeight(height) - metodo setter que atualiza altura definida. Parâmetro refere-se ao valor que irá substituir o atributo atual.`)
+console.log(`retangulo.setWidth(width) - metodo setter que atualiza largura definida. Parâmetro refere-se ao valor que irá substituir o atributo atual.`)
+console.log(`retangulo.getHeight() - metodo getter que retorna altura definida.`)
+console.log(`retangulo.getWidth() - metodo getter que retorna largura definida.`)
+console.log(`retangulo.calculatePerimeter - metodo que calcula área em metro². Usa os valores atuais dos atributos do objeto para fazer os cálculos.`)
+console.log(`retangulo.calculateArea() - metodo que calcula perímetro do retângulo e retornar valor em metros. Usa os valores atuais dos atributos do objeto para fazer os cálculos.`)
+
